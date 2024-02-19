43e5bcb8e6b3c9564787416b50294af47aa9d2a3
zhangxiaoqiang <zhangxiaoqiang@xigua.city>
2024-02-19 13:51:39 +0800

    fix: Promise.reject is not a constructor

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -1448,7 +1448,7 @@ class VirtualMachine extends EventEmitter {
             });
         }
         // If the target cannot be found by id, return a rejected promise
-        return new Promise.reject();
+        return Promise.reject();
     }
 
     /**
